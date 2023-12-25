e24fd232251e11ebd18d6a1e6f1d143fe05f49a2
khuongduy354 <42113313+khuongduy354@users.noreply.github.com>
2023-12-25 17:30:12 +0700

    feat: simple POC sync

---
+++ README.md
@@ -0,0 +1,6 @@
+# Issues 
+- Very slow (since whole array of objects) 
+- inconsistence: no proper syncing, just sync all from this 
+and broadcast the other
+- no middle sync: when new user enter, no sync. I mean we should have 
+file in middle server not on client.
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,42 @@
 import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
 const PORT = 8000;
 
 const app = express();
+const httpserver = createServer(app);
+const io = new Server(httpserver, {
+  cors: {
+    origin: "*",
+  },
+});
+// const rooms: Array<string> = [];
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+type LinePayload = {
+  line: number;
+  line_data: any;
+};
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  //create room
+  socket.on("join", (roomName: string, userName: string = "User ") => {
+    // if (!rooms.includes(roomName)) rooms.push(roomName);
+    socket.join(roomName);
+    io.to(roomName).emit("roomNotify", userName + " has joined the room");
+  });
+
+  //notification
+
+  // content changes
+  socket.on("contentChanges", (roomName: string, data) => {
+    console.log(data);
+    socket.broadcast.to(roomName).emit("contentChanges", data);
+  });
 });
 
-app.listen(PORT, () => {
+httpserver.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
